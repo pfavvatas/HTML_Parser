bd04c274c565ef0b59f06d964a27f2febc783ab4
panagiotis <panagiotis.favvatas@ist.com.gr>
2023-04-28 17:07:50 +0300

    fix utils

--- common/utils.py
+++ common/utils.py
@@ -8,6 +8,20 @@ setup_logging()
 # Create a logger for the module
 logger = logging.getLogger(__name__)
 
+def get_list(filename=None):
+    #print(filename)
+    return get_file_data(filename)
+
+def get_file_data(filename=None):
+    lines = []
+    # https://stackoverflow.com/questions/3925614/how-do-you-read-a-file-into-a-list-in-python
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            lines.append(line)
+            # print(line)
+    return lines
+
 def is_valid_url(url):
     logger.debug(f"Function {is_valid_url.__name__} is running with arguments {url}")
     """
