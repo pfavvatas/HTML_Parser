9748d7a54002ff104b92701afdfc0282af30359f
panagiotis <panagiotis.favvatas@ist.com.gr>
2023-04-28 12:55:01 +0300

    use logger

--- html_code/html.py
+++ html_code/html.py
@@ -1,10 +1,16 @@
 import os, time
+import logging
+from logs.log_setup import *
 from url_code import *
 from url_code.utils import *
 from html_code.parser import *
 from html_code.utils import *
 from common.utils import *
 
+# Set up the logging configuration
+setup_logging()
+# Create a logger for the module
+logger = logging.getLogger(__name__)
 
 List = []
 
@@ -14,14 +20,16 @@ def html_init_parser(argv):
     # urlList = readList(argv[0])
     List = readList(argv[0])
 
-    print(List)
+    logger.debug("List: %s", List)
     # from parser import Parse
     for item in List:
         # Parse(url)
         # html_data = get_file_html_data(item)
         if is_valid_url(item) :
+            logger.debug(item + " is a valid url")
             html_data = get_url_html_data(item)
         elif is_valid_file_path(item) :
+            logger.debug(item + " is a valid file path")
             html_data = get_file_html_data(item)
         #print(html_data)
         #HTMLParser
@@ -29,6 +37,6 @@ def html_init_parser(argv):
         # testParser.feed(html_data)        
 
 def readList(filename=None, **kwargs):
-    # print(filename)
+    logger.debug("filename: " , filename)
     return get_list(filename)
     
